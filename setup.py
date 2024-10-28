224c09da6843d928f21ee74f59fa5eb1f444c9d2
Anis Langar <al@lade.de>
2024-10-28 11:30:50 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BuddyAssistant',               
-    version='0.1.6',                   
+    version='0.1.7',                   
     description='BuddyAssistant is a conversational AI assistant.',  
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
